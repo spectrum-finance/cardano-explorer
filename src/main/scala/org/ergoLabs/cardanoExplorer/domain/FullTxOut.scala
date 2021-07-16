483e06a794ee18803d5e13167e50e07b8b849aff
alexander.romanovskiy <alexander.romanovskiy@corp.mail.ru>
2021-07-16 13:44:03 +0300

    fix naming

--- src/main/scala/org/ergoLabs/cardanoExplorer/domain/FullTxOut.scala
+++ src/main/scala/org/ergoLabs/cardanoExplorer/domain/FullTxOut.scala
@@ -6,7 +6,7 @@ import sttp.tapir.Schema
 
 @derive(customizableEncoder, customizableDecoder)
 final case class FullTxOut(
-  txId: TxId,
+  refId: TxId,
   refIdx: RefIdx,
   txOutAddress: Address,
   txOutValue: Value,
