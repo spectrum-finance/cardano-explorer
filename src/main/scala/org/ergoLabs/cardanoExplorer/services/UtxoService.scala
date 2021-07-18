8d1301128061782a1bb82f158920720867c4b75e
aleksandr <alexbromel@gmail.com>
2021-07-18 18:22:04 +0300

    update mock datum

--- src/main/scala/org/ergoLabs/cardanoExplorer/services/UtxoService.scala
+++ src/main/scala/org/ergoLabs/cardanoExplorer/services/UtxoService.scala
@@ -62,7 +62,7 @@ object UtxoService {
               AddressCredential(Contents("21fe31dfa154a261626bf854046fd2271b7bed4b6abe45aa58877ef47f9721b9"))
           ),
           Value(List.empty),
-          Datum1("d87989d879800a0a0a40d879824040d87982404040d879824040")
+          Datum1("d87988d879800a0a40d879824040d8798240405820a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434ad879824040")
         ),
         FullTxOut(
           Gid(2),
